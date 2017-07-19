9027441b0ddf4c71f05824e3f843c69bc97f48c8
Ty-Zeta <tyofzeta@gmail.com>
2017-07-19 10:15:45 -0400

    able to get odd # into first array, but doesn't work with even # of arrays

--- random_pairing.rb
+++ random_pairing.rb
@@ -1,4 +1,11 @@
 def pairing(names)
   pairs = names.shuffle.each_slice(2).to_a
+    last_one = pairs.pop
+    last_one = last_one[0]
+    pairs[0] << last_one
   p pairs
 end
+
+
+
+#pairs[0] << pairs[-1]
